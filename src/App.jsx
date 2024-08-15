d5fb3f97df6401bf6a1c575ed747e0a69f81133d
keicarriedo <erika.liongco@gmail.com>
2024-08-15 11:09:17 +0800

    storing component style files next to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import reactImg from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        { description } React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
 // USING PROPS
 /* function CoreConcept(props) {
@@ -43,17 +24,6 @@ function Header() {
     </li>
   );
 } */
-
-// PASSING A SINGLE PROP OBJECT
-function CoreConcept({ concept }) {
-  return (
-    <li>
-      <img src={concept.image} alt={concept.title} />
-      <h3>{concept.title}</h3>
-      <p>{concept.description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
