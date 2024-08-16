adc2411ae0a0983a7db0359e7beb4fc8785047f2
keicarriedo <erika.liongco@gmail.com>
2024-08-16 16:03:15 +0800

    Rendering content conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,27 @@ import { EXAMPLES } from './data.js';
 } */
 
 function App() {
-  const [ selectedTopic, setSelectedTopic] = useState('components');
+  const [ selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     // selectedButton => 'components', 'jsx', 'props', 'state'
     setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{ EXAMPLES[selectedTopic].title }</h3>
+        <p>{ EXAMPLES[selectedTopic].description }</p>
+        <pre>
+          <code>
+            { EXAMPLES[selectedTopic].code }
+          </code>
+        </pre>
+      </div>
+    );
   }
   return (
     <div>
@@ -86,15 +102,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-              <h3>{ EXAMPLES[selectedTopic].title }</h3>
-              <p>{ EXAMPLES[selectedTopic].description }</p>
-              <pre>
-                <code>
-                  { EXAMPLES[selectedTopic].code }
-                </code>
-              </pre>
-          </div>
+          { tabContent }
         </section> 
       </main>
     </div>
